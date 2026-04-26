160b7cc9a647118e3ab3860b0e96f436ef76b4bd
Danil <danila919danila@gmail.com>
2026-04-26 11:25:34 +0200

    Added Lesson30_1 folder

---
+++ CPP/Lesson30_1/Lesson30_1/Source.cpp
@@ -0,0 +1,44 @@
+﻿// Додавати об'єкт лише на початок. Забирати лише з початку. 
+// Якщо max_size більше -1 використовувати це значення як максимальне скільки може зберігати
+template<typename T>
+class Stack
+{
+    list<T> list;
+
+    const int max_size = -1;
+
+public:
+    Stack(int max_size)
+    {
+        this->max_size = max_size;
+    }
+
+    void Push(const T& item);
+    T Pop();
+    const T& Top() const;
+    bool Empty() const;
+    int Size() const;
+};
+
+// Додавати об'єкт лише на кінець. Забирати лише з початку. 
+// Якщо увімкнено ісКикліковий, то при забиранні об'єкта вставляти його в кінець
+template<typename T>
+class Queue
+{
+    list<T> list;
+
+    bool isCyclical;
+
+public:
+    Queue(bool isCyclical)
+    {
+        this->isCyclical = isCyclical;
+    }
+
+    void Push(const T& item);
+    T Pop();
+    const T& Front() const;
+    bool Empty() const;
+    int Size() const;
+
+};
