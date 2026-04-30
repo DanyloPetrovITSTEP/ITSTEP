3df0e2062b07d56a34d52f9bb7d1e2611640b5de
Danil <danila919danila@gmail.com>
2026-04-30 19:20:39 +0200

    Added "Lesson31" and "Lesson32" folders

---
+++ CPP/Lesson31/Lesson31/Source.cpp
@@ -0,0 +1,279 @@
+﻿#include <chrono>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+template<typename T>
+class Node
+{
+    T data;
+
+    Node<T>* parent;
+    Node<T>* next;
+    Node<T>* prev;
+
+    bool isBlack = false;
+
+public:
+    Node<T>*& GetParent() { return parent; }
+    void SetParent(Node<T>* parent) { this->parent = parent; }
+
+    Node<T>*& GetNext() { return next; }
+    Node<T>*& GetPrev() { return prev; }
+    void SetNext(Node<T>* next)
+    {
+        this->next = next;
+        if (prev != nullptr)
+        {
+            prev->SetParent(this);
+        }
+    }
+    void SetPrev(Node<T>* prev)
+    {
+        this->prev = prev;
+        if (prev != nullptr)
+        {
+            prev->SetParent(this);
+        }
+    }
+
+    bool IsBlack() const { return isBlack; }
+    void SetBlack(bool isBlack) { this->isBlack = isBlack; }
+
+    const T& GetData() const { return data; }
+    void SetData(const T& newData) { data = newData; }
+
+    Node(const T& data, Node<T>* next = nullptr, Node<T>* prev = nullptr, Node<T>* parent = nullptr)
+    {
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
+        this->parent = parent;
+    }
+};
+
+template<typename T>
+class BinaryTree
+{
+    Node<T>* root;
+
+    void DeleteTree(Node<T>*& node)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        DeleteTree(node->GetPrev());
+        DeleteTree(node->GetNext());
+
+        delete node;
+    }
+
+    void PrintTree(Node<T>* node) const
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        PrintTree(node->GetPrev());
+
+        cout << node->GetData() << endl;
+
+        PrintTree(node->GetNext());
+    }
+
+    void Rebalance(Node<T>* node)
+    {
+		while (node != root && node->GetParent()->IsBlack() == false) // виконуємо дії поки не дійдемо до кореня і поки батьківский вузел - червоний
+        {
+            Node<T>* parent = node->GetParent(); // "запам'ятовуємо" батьківський вузел
+			Node<T>* grand = parent->GetParent(); // "запам'ятовуємо" вузел перед батьківським, діда :P
+
+			if (!grand || !parent) return; // якщо немає діда або батька, то виходимо з функції
+
+            if (parent == grand->GetPrev()) // якщо батікьвский вузел є лівим вузлом діда тоді...
+            {
+				Node<T>* uncle = grand->GetNext(); // "запам'ятовуємо" правий вузел діда, дядько
+
+                if (uncle && uncle->IsBlack() == false) // якщо дядько існує і він червоний, тоді...
+                {
+                    parent->SetBlack(true); // робимо батьківский вузел чорним
+                    uncle->SetBlack(true); // робимо вузел "дядько" чорним
+                    grand->SetBlack(false); // робимо вузел "дід" червоним
+
+                    node = grand; // робимо вузел "дід" поточним для наступної ітерації
+                }
+                else  // або, якщо дядька немає, або він чорний (O_O), тоді...
+                {
+					if (node == parent->GetNext()) // якщо поточний вузел є правим вузлом батьківского вузла, тоді...
+                    {
+                        node = parent; // робимо поточни вузел - батьківским
+                        RotateLeft(node); // виконуємо зрушення вліво від поточного вузла, який тепер є батьківским
+                    }
+
+                    parent->SetBlack(true); // робимо батьківский вузел чорним (ПРАВИЛЬНО КАЗАТИ ТЕМНОШКІРИМ... а, ой, ми не про це..)
+                    grand->SetBlack(false); // робимо вузел діда червоним
+
+                    RotateRight(grand); // зрушуємо дерево вправо від вузла діда
+                }
+            }
+			else // якщо батьківский вузел не є лівим вузлом діда, тоді...
+            {
+				Node<T>* uncle = grand->GetPrev(); // запам'ятовуємо лівий вузел діда як дядька
+
+                if (uncle && uncle->IsBlack() == false) // якщо дядько існує і він червоний, тоді...
+                {
+					parent->SetBlack(true); // робимо батьківский вузел чорним
+                    uncle->SetBlack(true); // робимо вузел дядька чорним
+                    grand->SetBlack(false); // робимо вузел діда червоним
+
+                    node = grand; // робимо вузел діда поточним
+                }
+                else // або, якщо дядька немає, або він чорний, тоді...
+                {
+					if (node == parent->GetPrev()) // якщо поточний вузел є лівим вузлом батьківского вузла, тоді...
+                    {
+						node = parent; // робимо поточни вузел - батьківским
+                        RotateRight(node); // зрушуємо дерево вправо від поточного вузла, тобто від батьківского
+                    }
+
+					parent->SetBlack(true); // робимо батьківский вузел чорним
+					grand->SetBlack(false); // робимо вузел діда червоним
+
+                    RotateLeft(grand); // зрушуємо дерево вліво від вузла діда
+                }
+            }
+        }
+    }
+
+    void RotateLeft(Node<T>* node)
+    {
+        Node<T>* right = node->GetNext(); // запам'ятовуємо правий вузел від поточного вузла
+		node->SetNext(right->GetPrev()); // робимо правим вузлом поточного вузла, лівим вузлом правого вузла (чаво я тільки-но написав??)
+
+        if (right->GetPrev() != nullptr) // якщо лівий вузол правого вузла існує, тоді..
+        {
+            right->GetPrev()->SetParent(node); // робимо потойний вузел батьківским для лівого вузла правого вузла ( ._. ...)
+        }
+
+        right->SetParent(node->GetParent()); // робимо батьківским вузлом для правого вузла, батьківский вузел поточного вузла
+
+        if (node->GetParent() == nullptr) // якщо в поточного вузла немає батьківского вузла, тоді...
+        {
+            root = right; // робимо правий вузел коренем дерева
+        }
+        else if (node == node->GetParent()->GetPrev()) // або, якщо поточний вузел є лівим вузлом батьківского вузла поточного вузла (вузел з вузлом на вузлі за вузлами вузлів x_x), тоді...
+        {
+            node->GetParent()->SetPrev(right);
+        }
+        else
+        {
+            node->GetParent()->SetNext(right);
+        }
+
+        right->SetPrev(node);
+        node->SetParent(right);
+    }
+
+    void RotateRight(Node<T>* node)
+    {
+        Node<T>* left = node->GetPrev();
+        node->SetPrev(left->GetNext());
+
+        if (left->GetNext() != nullptr)
+        {
+            left->GetNext()->SetParent(node);
+        }
+
+        left->SetParent(node->GetParent());
+
+        if (node->GetParent() == nullptr)
+        {
+            root = left;
+        }
+
+        else if (node == node->GetParent()->GetNext())
+        {
+            node->GetParent()->SetNext(left);
+        }
+        else
+        {
+            node->GetParent()->SetPrev(left);
+        }
+
+        left->SetNext(node);
+        node->SetParent(left);
+    }
+
+public:
+    BinaryTree() : root(nullptr) {}
+    ~BinaryTree() { DeleteTree(root); }
+
+    void Insert(const T& data)
+    {
+        Node<T>* newNode = new Node<T>(data);
+
+
+        Node<T>* parent = nullptr;
+        Node<T>* current = root;
+
+        while (current)
+        {
+            parent = current;
+
+            if (data < current->GetData())
+            {
+                current = current->GetPrev();
+            }
+            else
+            {
+                current = current->GetNext();
+            }
+        }
+
+        newNode->SetParent(parent);
+
+        if (parent == nullptr)
+        {
+            root = newNode;
+        }
+        else if (data < parent->GetData())
+        {
+            parent->SetPrev(newNode);
+        }
+        else
+        {
+            parent->SetNext(newNode);
+        }
+
+        Rebalance(newNode);
+    }
+
+    void Print() const
+    {
+        PrintTree(root);
+        cout << endl;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    BinaryTree<int> tree;
+    tree.Insert(10);
+    tree.Insert(20);
+    tree.Insert(30);
+    tree.Insert(40);
+    tree.Insert(50);
+    tree.Insert(60);
+    tree.Insert(70);
+    tree.Insert(80);
+    tree.Insert(90);
+    tree.Insert(100);
+
+    tree.Print();
+
+    return 0;
+}
---
+++ CPP/Lesson32/Lesson32/Source.cpp
@@ -0,0 +1,267 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+class Scene;
+
+class Actor
+{
+    Scene* scene;
+
+public:
+    Scene* GetScene() const { return scene; }
+
+    Actor(Scene* scene)
+    {
+        this->scene = scene;
+    }
+
+    virtual void Tick() = 0;
+    virtual void Print() = 0;
+};
+
+class Scene
+{
+    std::vector<Actor*> actors;
+
+public:
+    void AddActor(Actor* actor)
+    {
+        actors.push_back(actor);
+    }
+
+    void RemoveActor(Actor* actor)
+    {
+        actors.erase(
+            std::remove(actors.begin(), actors.end(), actor),
+            actors.end()
+        );
+    }
+
+    const std::vector<Actor*>& GetActors() const { return actors; }
+
+    void Tick()
+    {
+        for (Actor* actor : actors)
+        {
+            actor->Tick();
+        }
+    }
+
+    void Print();
+};
+
+class Field : public Actor
+{
+    int grass;
+    int max_grass;
+    int grow_speed;
+
+public:
+    int GetGrass() const { return grass; }
+
+    int GatherGrass(const int value)
+    {
+        grass -= value;
+
+        if (grass >= 0) return value;
+
+        const int result = value + grass;
+
+        grass = 0;
+
+        return result;
+    }
+
+    Field(Scene* scene, const int grass, const int max_grass, const int grow_speed) : Actor(scene)
+    {
+        this->grass = std::min(grass, max_grass);
+        this->max_grass = max_grass;
+        this->grow_speed = grow_speed;
+    }
+
+    void Tick() override
+    {
+        grass = std::min(max_grass, grass + grow_speed);
+    }
+
+    void Print() override
+    {
+        std::cout << grass << '/' << max_grass << std::endl;
+    }
+};
+
+class Animal : public Actor
+{
+protected:
+    int hunger;
+    int max_hunger;
+    bool isAlive = true;
+
+public:
+    bool IsAlive() const { return isAlive; }
+
+    Animal(Scene* scene, const int hunger, const int max_hunger) : Actor(scene)
+    {
+        this->hunger = hunger;
+        this->max_hunger = max_hunger;
+    }
+
+    void Tick() override
+    {
+        --hunger;
+
+        if (hunger <= 0)
+        {
+            isAlive = false;
+        }
+    }
+
+    void Print() override
+    {
+        std::cout << hunger << '/' << max_hunger << std::endl;
+    }
+
+    void Eat(int value)
+    {
+        hunger = std::min(max_hunger, hunger + value);
+    }
+};
+
+class Rabbit : public Animal
+{
+public:
+    Rabbit(Scene* scene, const int hunger, const int max_hunger) : Animal(scene, hunger, max_hunger) {}
+
+    void Tick() override
+    {
+        Animal::Tick();
+
+        int need_huger = max_hunger - hunger;
+
+        for (Actor* actor : GetScene()->GetActors())
+        {
+            Field* field = dynamic_cast<Field*>(actor);
+
+            if (field == nullptr) continue;
+
+            int gathered = field->GatherGrass(need_huger);
+            Eat(gathered);
+
+            need_huger -= gathered;
+
+            if (need_huger <= 0) break;
+        }
+    }
+};
+
+void Scene::Print()
+{
+    int cout_grass = 0;
+    int cout_rabbits = 0;
+
+    for (Actor* actor : actors)
+    {
+        if (Field* field = dynamic_cast<Field*>(actor))
+        {
+            cout_grass += field->GetGrass();
+        }
+        else if (Rabbit* rabbit = dynamic_cast<Rabbit*>(actor))
+        {
+            if (rabbit->IsAlive())
+            {
+                cout_rabbits++;
+            }
+        }
+    }
+
+    std::cout << "Grass: " << cout_grass << '\n' << "Rabbits: " << cout_rabbits << '\n';
+}
+
+int main()
+{
+    Scene* scene = new Scene();
+
+    // === Fields Actors ===
+
+    std::vector<Field*> fields;
+
+    fields.reserve(5);
+    for (size_t i = 0; i < 5; i++)
+    {
+        fields.push_back(new Field(
+            scene,
+            std::rand() % 20 + 1,
+            std::rand() % 20 + 1,
+            std::rand() % 3 + 1
+        ));
+    }
+
+    // === Fields Actors ===
+
+    std::vector<Rabbit*> rabbits;
+
+    rabbits.reserve(15);
+    for (size_t i = 0; i < 15; i++)
+    {
+        const int max_hunger = std::rand() % 10 + 5;
+        rabbits.push_back(new Rabbit(scene, max_hunger, max_hunger));
+    }
+
+    // =======
+
+    for (Actor* actor : fields)
+    {
+        scene->AddActor(actor);
+    }
+
+    for (Actor* actor : rabbits)
+    {
+        scene->AddActor(actor);
+    }
+
+    // ======
+
+
+    for (int i = 0; i < 60; i++)
+    {
+        scene->Tick();
+
+        system("cls");
+
+        std::cout << "Tick: " << i << std::endl;
+        scene->Print();
+
+
+
+        /*std::cout << " : Fields" << std::endl;
+        for (Actor* actor : fields)
+        {
+            actor->Print();
+        }
+        std::cout << std::endl;
+
+        std::cout << " : Rabbits" << std::endl;
+        for (Actor* actor : rabbits)
+        {
+            actor->Print();
+        }
+        std::cout << std::endl;*/
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+
+
+
+
+
+
+
+    // ========
+
+    delete scene;
+
+    return 0;
+}
